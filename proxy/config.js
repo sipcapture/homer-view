2c28ba3299b48e8a86adaaf93005bd93200060a9
Eduard Klimenko <edik.klimenko@gmail.com>
2020-03-20 12:35:16 +0200

    needs configure before use PROXY

--- proxy/config.js
+++ proxy/config.js
@@ -1,23 +1,10 @@
-// [HEPIC]
-// var config = {
-//         apiUrl: 'http://de7.sipcapture.io/',
-//         apiSess: 'http://de7.sipcapture.io/api/v2/session',
-//         apiAuthJWT: false, // Activate for Homer7 setup
-//         apiUser: 'admin',
-//         apiPass: 'garbage',
-//         timeOut: 1800, // seconds
-//         proxyHost: '0.0.0.0',
-//         proxyPort: 8765
-// };
-
-// [HOMER]
 var config = {
         debug: true,
-        apiUrl: 'http://de9.sipcapture.io/',
-        apiSess: 'http://de9.sipcapture.io/api/v3/auth',
-        apiAuthJWT: true, // Activate for Homer7 setup
-        apiUser: 'admin',
-        apiPass: 'sipcapture',
+        apiUrl: 'http://[your-homer-app-domain]/',
+        apiSess: 'http://[your-homer-app-domain]/api/v3/auth',
+        apiAuthJWT: true,
+        apiUser: '[user-name]',
+        apiPass: '[password]',
         timeOut: 1800, // seconds
         proxyHost: '0.0.0.0',
         proxyPort: 8765
